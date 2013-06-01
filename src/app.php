61941c50c562d3a73d95d715319f8fbde5385b3f
Geoff Appleby <geoff@gapple.ca>
2013-05-31 22:22:10 -0700

    Add simple about page

--- src/app.php
+++ src/app.php
@@ -3,6 +3,11 @@ require_once('bootstrap.php');
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig', array(
+    ));
+});
+
+$app->get('/about', function () use ($app) {
+    return $app['twig']->render('about.twig', array(
 
     ));
 });
