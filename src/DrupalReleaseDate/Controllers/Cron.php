67aeaf02e59310df01b359699cde69b8a57f34a7
Geoff Appleby <geoff@gapple.ca>
2013-06-07 07:58:39 -0700

    Restore setting empty date on estimation failure

--- src/DrupalReleaseDate/Controllers/Cron.php
+++ src/DrupalReleaseDate/Controllers/Cron.php
@@ -62,6 +62,7 @@ class Cron
         }
         else if ($estimateDuration === 0) {
             $update += array(
+                $app['db']->quoteIdentifier('estimate') => '0000-00-00 00:00:00',
                 $app['db']->quoteIdentifier('note') => 'Run failed due to increasing issue count',
             );
         }
