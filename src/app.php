16debf92ec5c954d80f60d054ae9029b48a931b7
Geoff Appleby <geoff@gapple.ca>
2013-06-06 18:45:28 -0700

    Organize cron routes into a ControllerCollection

--- src/app.php
+++ src/app.php
@@ -7,13 +7,14 @@ use Silex\Application;
 $app->get('/', 'DrupalReleaseDate\Controllers\Pages::index');
 $app->get('about', 'DrupalReleaseDate\Controllers\Pages::about');
 
-$app->get('cron', function () {
-  return '';
-});
 
+$cron = $app['controllers_factory'];
+$cron->get('/', 'DrupalReleaseDate\Controllers\Cron::emptyResponse');
 
 // Handle request to update estimate value, protected by key.
-$app->get('cron/update-estimate', 'DrupalReleaseDate\Controllers\Cron::emptyResponse');
-$app->get('cron/update-estimate/{key}', 'DrupalReleaseDate\Controllers\Cron::updateEstimate');
+$cron->get('/update-estimate', 'DrupalReleaseDate\Controllers\Cron::emptyResponse');
+$cron->get('/update-estimate/{key}', 'DrupalReleaseDate\Controllers\Cron::updateEstimate');
+
+$app->mount('/cron', $cron);
 
 $app->run();
