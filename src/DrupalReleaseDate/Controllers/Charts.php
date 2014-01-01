1477995c1b7042b587ed2b988aef6caf0fa89c9c
Geoff Appleby <geoff@gapple.ca>
2014-01-01 14:39:11 -0800

    Fix path name for chart twig templates

--- src/DrupalReleaseDate/Controllers/Charts.php
+++ src/DrupalReleaseDate/Controllers/Charts.php
@@ -9,7 +9,7 @@ class Charts
 
     public function samples(Application $app, Request $request)
     {
-        return $app['twig']->render('graphs/samples.twig', array(
+        return $app['twig']->render('charts/samples.twig', array(
             'scripts' => array(
                 '//code.jquery.com/jquery-2.0.2.min.js',
                 'https://www.google.com/jsapi',
@@ -19,7 +19,7 @@ class Charts
 
     public function estimates(Application $app, Request $request)
     {
-        return $app['twig']->render('graphs/estimates.twig', array(
+        return $app['twig']->render('charts/estimates.twig', array(
             'scripts' => array(
                 '//code.jquery.com/jquery-2.0.2.min.js',
                 'https://www.google.com/jsapi',
@@ -29,7 +29,7 @@ class Charts
 
     public function distribution(Application $app, Request $request)
     {
-        return $app['twig']->render('graphs/distribution.twig', array(
+        return $app['twig']->render('charts/distribution.twig', array(
             'scripts' => array(
                 '//code.jquery.com/jquery-2.0.2.min.js',
                 'https://www.google.com/jsapi',
