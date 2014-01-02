682875dce476ae9223f3e0adf6beabad4fa33bfa
Geoff Appleby <geoff@gapple.ca>
2014-01-01 22:43:12 -0800

    Fix Cache-Control directives for home page
    Previously all pages were cached for two weeks, but the homepage needs to
    reflect the estimate update from the current day

--- src/DrupalReleaseDate/Controllers/PagesControllerProvider.php
+++ src/DrupalReleaseDate/Controllers/PagesControllerProvider.php
@@ -12,14 +12,20 @@ class PagesControllerProvider implements ControllerProviderInterface
     {
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('/', 'DrupalReleaseDate\Controllers\Pages::index');
-        $controllers->get('about', 'DrupalReleaseDate\Controllers\Pages::about');
-
-        $controllers->after(function(Request $request, Response $response) {
-            // Allow caching for one week.
-            $response->setMaxAge(604800);
-            $response->setSharedMaxAge(604800);
-        });
+        $controllers
+            ->get('/', 'DrupalReleaseDate\Controllers\Pages::index')
+            ->after(function(Request $request, Response $response) {
+                // Allow caching for one hour.
+                $response->setMaxAge(3600);
+                $response->setSharedMaxAge(3600);
+            });
+        $controllers
+            ->get('about', 'DrupalReleaseDate\Controllers\Pages::about')
+            ->after(function(Request $request, Response $response) {
+                // Allow caching for one week.
+                $response->setMaxAge(604800);
+                $response->setSharedMaxAge(604800);
+            });
 
         return $controllers;
     }
