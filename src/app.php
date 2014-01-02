a10d4ea79330ff812f019769f1626ef7cb03d32d
Geoff Appleby <geoff@gapple.ca>
2014-01-01 23:19:03 -0800

    app.php doesn't need any use statements anymore

--- src/app.php
+++ src/app.php
@@ -1,11 +1,6 @@
 <?php
 require_once('bootstrap.php');
 
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
 $app->mount('/', new \DrupalReleaseDate\Controllers\PagesControllerProvider());
 $app->mount('/chart', new \DrupalReleaseDate\Controllers\ChartsControllerProvider());
 $app->mount('/data', new \DrupalReleaseDate\Controllers\DataControllerProvider());
