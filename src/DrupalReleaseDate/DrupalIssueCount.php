6d858f2bac8df523ff38c863dca788bed6a834a2
Geoff Appleby <geoff@gapple.ca>
2014-04-07 21:59:13 -0700

    Use Symfony DomCrawler component for parsing Drupal.org pages

--- src/DrupalReleaseDate/DrupalIssueCount.php
+++ src/DrupalReleaseDate/DrupalIssueCount.php
@@ -1,6 +1,8 @@
 <?php
 namespace DrupalReleaseDate;
 
+use Symfony\Component\DomCrawler;
+
 class DrupalIssueCount
 {
     protected $client;
@@ -121,7 +123,7 @@ class DrupalIssueCount
             }
             try {
                 $results[$fetchKey] = $this->getCount($request);
-            } catch (DrupalOrgParseException $e) {
+            } catch (Exception $e) {
                 $results[$fetchKey] = null;
             }
         }
@@ -140,89 +142,35 @@ class DrupalIssueCount
      */
     public function getCount(\Guzzle\Http\Message\RequestInterface $request)
     {
-
         // Make sure page isn't set from a previous call on the same request object.
         $request->getQuery()->remove('page');
 
-        $document = $this->getXmlDocument($request->send());
-
-        // Check if pager exists on first page; get the next page until we're at
-        // the end to find the total number of pages.
-        $fullPages = 0;
-        $pagerNext = $document->xpath(
-            "//_xmlns:div[contains(concat(' ', @class, ' '), ' view-project-issue-search-project-searchapi ')]//_xmlns:li[contains(concat(' ', @class, ' '), ' pager-next ')]//_xmlns:a"
-        );
-
-        while ($pagerNext) {
-            $pagerNextUrl = (string) $pagerNext[0]['href'];
-            preg_match('/page=(\\d+)/', $pagerNextUrl, $urlMatches);
-
-            $fullPages = (int) $urlMatches[1]; // Pager starts at 0,
-            $request->getQuery()->set('page', $fullPages);
-            $document = $this->getXmlDocument($request->send());
-
-            $pagerNext = $document->xpath(
-                "//_xmlns:div[contains(concat(' ', @class, ' '), ' view-project-issue-search-project-searchapi ')]//_xmlns:li[contains(concat(' ', @class, ' '), ' pager-next ')]//_xmlns:a"
-            );
-        }
-
-        $issueRows = $document->xpath(
-            "//_xmlns:div[contains(concat(' ', @class, ' '), ' view-project-issue-search-project-searchapi ')]//_xmlns:table[contains(concat(' ', @class, ' '), ' views-table ')]/_xmlns:tbody/_xmlns:tr"
-        );
-
-        // Drupal.org is returning rows where all cells are empty, which bumps
-        // up the count incorrectly.
         $issueRowCount = 0;
-        foreach ($issueRows as $issueRow) {
-            if (!empty($issueRow->td[0]->a)) {
-                $issueRowCount++;
+
+        while(true) {
+            $document = new DomCrawler\Crawler((string) $request->send()->getBody());
+            $issueView = $document->filter('.view-project-issue-search-project-searchapi');
+
+            $issueRowCount += $issueView
+                ->filter('table.views-table tbody tr')
+                ->reduce(function (DomCrawler\Crawler $element) {
+                    // Drupal.org is returning rows where all cells are empty,
+                    // which bumps up the count incorrectly.
+                    return $element->filter('td')->first()->filter('a')->count() > 0;
+                })
+                ->count();
+
+            $pagerNext = $issueView->filter('.pager-next a');
+
+            if (!$pagerNext->count()) {
+                break;
             }
-        }
 
-        $issues = $issueRowCount + 50 * $fullPages;
+            preg_match('/page=(\\d+)/', $pagerNext->attr('href'), $urlMatches);
 
-        return $issues;
-    }
+            $request->getQuery()->set('page', (int) $urlMatches[1]);
+        };
 
-    /**
-     * Fetch a page with the specied parameters, and return the resulting
-     * document as a SimpleXMLElement.
-     *
-     * @param array $parameters
-     * @return \SimpleXMLElement
-     */
-    public function getXmlDocument(\Guzzle\Http\Message\Response $response)
-    {
-
-        try {
-            libxml_use_internal_errors(true);
-            $document = $response->xml();
-        } catch (\Guzzle\Common\Exception\RuntimeException $e) {
-            libxml_clear_errors();
-
-            // Drupal.org may return invalid XML due to unescaped ampersands and
-            // non-breaking spaces (which aren't valid in XHTML serialization of
-            // HTML5).
-            $page = (string) $response->getBody();
-
-            $replace = array(
-                ' & ' => ' &amp; ',
-                '&nbsp;' => '&#xA0;',
-            );
-            $page = str_replace(array_keys($replace), array_values($replace), $page);
-
-            try {
-                $document = new \SimpleXMLElement($page);
-            } catch (\Exception $e) {
-                // Throw a better specified Exception.
-                throw new DrupalOrgParseException();
-            }
-        }
-
-        // Need to register name for default namespace to be able to query anything with xpath
-        $namespaces = $document->getDocNamespaces();
-        $document->registerXPathNamespace('_xmlns', $namespaces['']);
-
-        return $document;
+        return $issueRowCount;
     }
 }
